40ac8c3ed78d15cced3bdb97569e80642b197db4
Melissa9mpenzi <melissampenzi@gmail.com>
2025-01-16 15:55:22 +0300

    added an about page and style page for css

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3a$6+o0hf+%&e0dbh$hmu2++&6b+%_a!gbtkx0y_(v7_6+a90a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
