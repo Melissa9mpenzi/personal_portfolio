ccd5ed2a858a5924de6da1392fc28b8d9583b516
Melissa9mpenzi <melissampenzi@gmail.com>
2025-01-16 10:21:37 +0300

    added content in models, urls and admin files

--- port_app/models.py
+++ port_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Service(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
