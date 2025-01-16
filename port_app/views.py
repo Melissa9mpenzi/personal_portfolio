d5c0a897686214a5c3fc23b8714dad11c1511318
Melissa9mpenzi <melissampenzi@gmail.com>
2025-01-16 10:40:04 +0300

    added forms, views and base files

--- port_app/views.py
+++ port_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Skill, Service
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def about(request):
+    skills = Skill.objects.all()
+    return render(request, 'core/about.html', {'skills': skills})
+
+def services(request):
+    services = Service.objects.all()
+    return render(request, 'core/services.html', {'services': services})
+
+def resume(request):
+    return render(request, 'core/resume.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
