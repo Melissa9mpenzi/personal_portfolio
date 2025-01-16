bd0fcd1f762116b0e6fa105c0b626d7d68d46b5d
Melissa9mpenzi <melissampenzi@gmail.com>
2025-01-16 10:51:45 +0300

    added index file, modified urls, views files

--- port_app/views.py
+++ port_app/views.py
@@ -4,18 +4,18 @@ from .models import Skill, Service
 # Create your views here.
 
 def index(request):
-    return render(request, 'core/index.html')
+    return render(request, 'port_app/index.html')
 
 def about(request):
     skills = Skill.objects.all()
-    return render(request, 'core/about.html', {'skills': skills})
+    return render(request, 'port_app/about.html', {'skills': skills})
 
 def services(request):
     services = Service.objects.all()
-    return render(request, 'core/services.html', {'services': services})
+    return render(request, 'port_app/services.html', {'services': services})
 
 def resume(request):
-    return render(request, 'core/resume.html')
+    return render(request, 'port_app/resume.html')
 
 def contact(request):
-    return render(request, 'core/contact.html')
+    return render(request, 'port_app/contact.html')
